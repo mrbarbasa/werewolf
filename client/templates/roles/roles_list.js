5a5da38d92031fc530887f17d65a537cf5e547d9
Marifel <mrbarbasa@gmail.com>
2015-03-07 19:39:46 -1000

    Set up Mongo collection for roles

--- client/templates/roles/roles_list.js
+++ client/templates/roles/roles_list.js
@@ -1,29 +1,5 @@
-var rolesData = [
-  {
-    name: 'Villager 1'
-  },
-  {
-    name: 'Villager 2'
-  },
-  {
-    name: 'Villager 3'
-  },
-  {
-    name: 'Villager 4'
-  },
-  {
-    name: 'Villager 5'
-  },
-  {
-    name: 'Evildoer 1'
-  },
-  {
-    name: 'Evildoer 2'
-  },
-  {
-    name: 'Neutral 1'
-  }
-];
 Template.rolesList.helpers({
-  roles: rolesData
+  roles: function() {
+    return Roles.find();
+  }
 });
---
+++ server/fixtures.js
@@ -0,0 +1,33 @@
+if (Roles.find().count() === 0) {
+  Roles.insert({
+    name: 'Villager 1'
+  });
+
+  Roles.insert({
+    name: 'Villager 2'
+  });
+
+  Roles.insert({
+    name: 'Villager 3'
+  });
+
+  Roles.insert({
+    name: 'Villager 4'
+  });
+
+  Roles.insert({
+    name: 'Villager 5'
+  });
+
+  Roles.insert({
+    name: 'Evildoer 1'
+  });
+
+  Roles.insert({
+    name: 'Evildoer 2'
+  });
+
+  Roles.insert({
+    name: 'Neutral 1'
+  });
+}
