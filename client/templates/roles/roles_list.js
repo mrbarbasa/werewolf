6f19658473ac8d0f2352748da412c738153ac934
Marifel <mrbarbasa@gmail.com>
2015-03-07 19:02:09 -1000

    Meteor project setup

---
+++ client/templates/roles/roles_list.js
@@ -0,0 +1,29 @@
+var rolesData = [
+  {
+    name: 'Villager 1'
+  },
+  {
+    name: 'Villager 2'
+  },
+  {
+    name: 'Villager 3'
+  },
+  {
+    name: 'Villager 4'
+  },
+  {
+    name: 'Villager 5'
+  },
+  {
+    name: 'Evildoer 1'
+  },
+  {
+    name: 'Evildoer 2'
+  },
+  {
+    name: 'Neutral 1'
+  }
+];
+Template.rolesList.helpers({
+  roles: rolesData
+});
