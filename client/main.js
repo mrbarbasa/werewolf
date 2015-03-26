dbb75bdba02d4b393100c8c404a318135721328c
Marifel <mrbarbasa@gmail.com>
2015-03-25 20:42:39 -1000

    Remove Quick Game option from main menu

--- client/main.js
+++ client/main.js
@@ -2,21 +2,3 @@ Meteor.subscribe('users');
 Meteor.subscribe('players');
 Meteor.subscribe('rooms');
 Meteor.subscribe('roles');
-
-Template.main.events({
-  'click #quick-game': function() {
-    console.log('quick game');
-  },
-  'click #join-game': function() {
-    console.log('join game');
-  },
-  'click #host-game': function() {
-    console.log('host game');
-  },
-  'click #view-profile': function() {
-    console.log('view profile');
-  },
-  'click #game-options': function() {
-    console.log('game options');
-  }
-});
