1e2ff17ca8fb9621b5136d63ea0f6dd169f6d610
Marifel <mrbarbasa@gmail.com>
2015-04-03 18:16:53 -1000

    Fix button styles on main menu and host game pages

--- client/templates/rooms/room_host.js
+++ client/templates/rooms/room_host.js
@@ -1,9 +1,6 @@
 Template.roomHost.events({
-  'submit form#host-game-form': function(e) {
-    e.preventDefault();
-
-    var roomName = $(e.target).find('[name=host-game-name]').val();
-    console.log(roomName);
+  'click #host-game-create': function() {
+    var roomName = $('#host-game-name').val();
     Meteor.call('roomCreate', roomName, function(err, result) {
       if (err) {
         return alert('Could not create room');
